4b29c8dd530bbb24bc2ceb9f348a6fed66f7cdb7
peti <petikoch@gmail.com>
2015-05-07 10:20:33 +0200

    initial project structure

---
+++ src/main/groovy/ch/petikoch/examples/feedback_control_java/FeedbackControlInJavaMain.java
@@ -0,0 +1,17 @@
+/**
+ * Copyright 2015 Peti Koch, All rights reserved.
+ *
+ * Project Info:
+ * https://github.com/Petikoch/feedback_control_bookexamples_in_java
+ *
+ * License:
+ * http://creativecommons.org/licenses/by-sa/4.0/deed.en
+ */
+package ch.petikoch.examples.feedback_control_java;
+
+public class FeedbackControlInJavaMain {
+
+    public static void main(String[] args) {
+        System.out.println("Yep");
+    }
+}
